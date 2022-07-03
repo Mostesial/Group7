281e3d1308819eae35f9747516d19fb7c82c2c96
Mostesial <3200100301@zju.edu.cn>
2022-07-03 16:18:51 +0800

    整合准备

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,29 @@
-// pages/index/index.js
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    username: '',
+    password: '',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  
+  inputUsername(e) {
+    this.setData({
+      username: e.detail.value,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  inputPassword(e) {
+    this.setData({
+      password: e.detail.value,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+  login(){
+    wx.navigateTo({
+      url: '../message/message?message='+this.data.username,
+    })
 
   }
+
 })
+
