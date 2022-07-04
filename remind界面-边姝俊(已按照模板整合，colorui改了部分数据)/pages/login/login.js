596038464f7415873cd9d498f0a53a5090531f5c
batboyBSJ <108111192+batboyBSJ@users.noreply.github.com>
2022-07-04 15:47:33 +0800

    Add files via upload

---
+++ remind界面-边姝俊(已按照模板整合，colorui改了部分数据)/pages/login/login.js
@@ -0,0 +1,85 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    username: '',
+    password: '',
+    animationlogo: {},
+    animationpurpleC1:{},
+    animationpurpleC2:{},
+    animationredC:{},
+    animationpinkC:{}
+  },
+  
+  inputUsername(e) {
+    this.setData({
+      username: e.detail.value,
+    })
+  },
+  inputPassword(e) {
+    this.setData({
+      password: e.detail.value,
+    })
+  },
+
+  onShow: function(){
+    var animation1 = wx.createAnimation({
+      duration: 2000,
+      timingFunction: 'linear',
+    })
+    var animation2 = wx.createAnimation({
+      duration: 2000,
+      timingFunction: 'linear',
+    })
+    var animation3 = wx.createAnimation({
+      duration: 2000,
+      timingFunction: 'linear',
+    })
+    var animation4 = wx.createAnimation({
+      duration: 2000,
+      timingFunction: 'linear',
+    })
+    var animation5 = wx.createAnimation({
+      duration: 700,
+      timingFunction: 'linear',
+    })
+    this.animation = animation1
+    setTimeout(function() {
+      animation1.translate(-100,100).step()
+      this.setData({
+        animationpurpleC1:animation1.export()
+      })
+    }.bind(this), 200)
+    this.animation = animation2
+    setTimeout(function() {
+      animation2.translate(20,-400).step()
+      this.setData({
+        animationpurpleC2:animation2.export()
+      })
+    }.bind(this), 200)
+    this.animation = animation3
+    setTimeout(function() {
+      animation3.translate(-70,0).step()
+      this.setData({
+        animationredC:animation3.export()
+      })
+    }.bind(this), 200)
+    this.animation = animation4
+    setTimeout(function() {
+      animation4.translate(-80,-450).step()
+      this.setData({
+        animationpinkC:animation4.export()
+      })
+    }.bind(this), 200)
+    this.animation = animation5
+    setTimeout(function() {
+      animation5.scale(0.7,0.7).step()
+      this.setData({
+        animationlogo:animation5.export()
+      })
+    }.bind(this), 200)
+  },
+})
+
---
+++ remind界面-边姝俊(已按照模板整合，colorui改了部分数据)/pages/logs/logs.js
@@ -0,0 +1,37 @@
+// logs.js
+Page({
+  data: {
+    sex:['妈妈','爸爸'],
+    babysex:['男孩','女孩'],
+    babytime:['32周','33周','34周','35周','36周','37周','38周','39周','40周','41周','42周','43周','44周','45周','46周','47周','48周'],
+    index1:0,
+    index2:0,
+    index3:8,
+    date:2022
+  },
+  // 事件处理函数
+  bindPickerChange1(e){
+    console.log(e);
+    this.setData({
+      index1:e.detail.value
+    })
+  },
+  bindPickerChange2(e){
+    console.log(e);
+    this.setData({
+      index2:e.detail.value
+    })
+  },
+  bindDateChange(e){
+    console.log(e);
+    this.setData({
+      date:e.detail.value
+    })
+  },
+  bindPickerChange3(e){
+    console.log(e);
+    this.setData({
+      index3:e.detail.value
+    })
+  }
+})
