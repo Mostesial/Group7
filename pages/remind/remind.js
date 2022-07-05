81e293a34d45cc54bd05562549695e002e211936
Mostesial <3200100301@zju.edu.cn>
2022-07-05 15:37:42 +0800

    紧急补添了remind界面

--- pages/remind/remind.js
+++ pages/remind/remind.js
@@ -1,66 +1,38 @@
 // pages/remind/remind.js
+
+// 待办事项
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    PageCur: 'basics'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    // 生命周期函数--监听页面加载
+    onLoad: function (options) {},
 
-  },
+    //生命周期函数--监听页面初次渲染完成
+    onReady: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    //生命周期函数--监听页面显示
+    onShow: function () {},
 
-  },
+    //生命周期函数--监听页面隐藏
+    onHide: function () {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    //生命周期函数--监听页面卸载
+    onUnload: function () {},
 
-  },
+    //页面相关事件处理函数--监听用户下拉动作
+    onPullDownRefresh: function () {},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    //页面上拉触底事件的处理函数
+    onReachBottom: function () {},
 
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    //用户点击右上角分享
+    onShareAppMessage: function () {}
 })
